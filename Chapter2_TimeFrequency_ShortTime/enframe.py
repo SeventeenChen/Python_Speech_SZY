5c04ddb94dff0b5c2772ec18e7f2b0fe6fac86d4
Your Name <you@example.com>
2020-07-28 00:20:52 +0800

    Initial commit

---
+++ Chapter2_TimeFrequency_ShortTime/enframe.py
@@ -0,0 +1,62 @@
+from scipy.io import wavfile
+import numpy as np
+import matplotlib.pyplot as plt
+
+def enframe(x, win, inc=None):
+    nx = len(x)
+    if isinstance(win, list):
+        nwin = len(win)
+        nlen = nwin  # 帧长=窗长
+    elif isinstance(win, int):
+        nwin = 1
+        nlen = win  # 设置为帧长
+    if inc is None:
+        inc = nlen
+    nf = (nx - nlen + inc) // inc	# 计算帧数
+    frameout = np.zeros((nf, nlen))		# 初始化
+    indf = np.multiply(inc, np.array([i for i in range(nf)]))	# 设置每帧在x中的位移量位置
+    for i in range(nf):
+        frameout[i, :] = x[indf[i]:indf[i] + nlen]		# 分帧
+    if isinstance(win, list):
+        frameout = np.multiply(frameout, np.array(win))		# 每帧乘以窗函数的值
+    return frameout
+
+
+fs, data = wavfile.read('bluesky3.wav')
+
+inc = 100
+wlen = 200
+en = enframe(data, wlen, inc)
+i = input('Start frame(i):')
+i = int(i)
+tlabel = i
+
+plt.subplot(4, 1, 1)
+x = [i for i in range((tlabel - 1) * inc, (tlabel - 1) * inc + wlen)]
+plt.plot(x, en[tlabel, :])
+plt.xlim([(i - 1) * inc + 1, (i + 2) * inc + wlen])
+plt.title('(a)The {} Frame Waveform'.format(tlabel))
+
+plt.subplot(4, 1, 2)
+x = [i for i in range((tlabel + 1 - 1) * inc, (tlabel + 1 - 1) * inc + wlen)]
+plt.plot(x, en[i + 1, :])
+plt.xlim([(i - 1) * inc + 1, (i + 2) * inc + wlen])
+plt.title('(b)The {} Frame Waveform'.format(tlabel + 1))
+
+plt.subplot(4, 1, 3)
+x = [i for i in range((tlabel + 2 - 1) * inc, (tlabel + 2 - 1) * inc + wlen)]
+plt.plot(x, en[i + 2, :])
+plt.xlim([(i - 1) * inc + 1, (i + 2) * inc + wlen])
+plt.title('(c)The {} Frame Waveform'.format(tlabel + 2))
+
+plt.subplot(4, 1, 4)
+x = [i for i in range((tlabel + 3 - 1) * inc, (tlabel + 3 - 1) * inc + wlen)]
+plt.plot(x, en[i + 3, :])
+plt.xlim([(i - 1) * inc + 1, (i + 2) * inc + wlen])
+plt.title('(d)The {} Frame Waveform'.format(tlabel + 3))
+
+
+plt.savefig('images/enframe.png')
+plt.show()
+plt.close()
+
---
+++ Chapter2_TimeFrequency_ShortTime/window.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+N = 32
+nn = [i for i in range(N)]
+plt.figure(figsize=(16, 12))
+plt.subplot(3, 1, 1)
+plt.stem(np.ones(N))
+plt.title('Rectangle window')
+
+# w = 0.54 - 0.46 * np.cos(np.multiply(nn, 2 * np.pi) / (N - 1))
+w = np.hamming(N)
+plt.subplot(3, 1, 2)
+plt.stem(w)
+plt.title('Hamming window')
+
+# w = 0.5 * (1 - np.cos(np.multiply(nn, 2 * np.pi) / (N - 1)))
+w = np.hanning(N)
+plt.subplot(3, 1, 3)
+plt.stem(w)
+plt.title('Hanning window')
+plt.savefig('images/window.png')
+plt.show()
+plt.close()
